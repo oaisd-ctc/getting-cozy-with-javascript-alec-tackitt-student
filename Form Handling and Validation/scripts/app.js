a4570903d36fa1aad4bdfc420bb1cf00f708d0b8
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2024-09-09 16:46:43 +0000

    Finished form handling

---
+++ Form Handling and Validation/scripts/app.js
@@ -0,0 +1,28 @@
+document.getElementById("submit").addEventListener("click", CheckForm, false);
+
+function CheckForm(evt){
+    var uname = document.getElementById("uname").value;
+    var email = document.getElementById("email").value;
+    var age = document.getElementById("age").value;
+    
+    if(uname.length < 3){
+        alert("Error: username needs to be at least 3 charecter");
+        evt.preventDefault();
+        return false;
+    }
+    else if (!email.includes('@') || !email.includes('.')){ 
+        alert("Error: email must contain at least 1 @ and .");
+        evt.preventDefault();
+        return false;
+    } 
+    else if (!(10 < age && age < 100)){
+        alert("Error: age must be between 10-100");
+        evt.preventDefault();
+        return false;
+    }
+    else{
+        alert("validations passed");
+        return true;
+    }
+
+}
