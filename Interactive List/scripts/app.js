0e446725394aa2bef80946dc8e42fb40204b4aa9
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2024-09-10 16:56:48 +0000

    Finished Interactive list

--- Interactive List/scripts/app.js
+++ Interactive List/scripts/app.js
@@ -1,29 +1,23 @@
 document.getElementById("submit").addEventListener("click", AddText);
-document.getElementByClass("Delete-Button").addEventListener("click", RemoveItem)
 
 function AddText(){
     // grabbing text from input
     var text = document.getElementById("text").value;
     //grabbing list
     var list = document.getElementById("list");
-    //creating a new list item and putting text in it
+    //creating a new list item and putting text in it and assiging uniqe id
     var item = document.createElement("li");
     item.textContent = text;
     //add new list item to list
     list.appendChild(item);
 
-    //creating new button
+    //creating new button and assigning uniqe id
     var button = document.createElement("button");
-    button.classList.add("Delete-Button");
     button.textContent = "Delete";
     list.appendChild(button);
 
-
-
- // neeed to ask martinez abt type of error message for form valdation
- //ask marinez aby keegan button layout
-}
-
-function RemoveItem(){
-    alert("deleting item");
+    button.addEventListener("click", () => {
+        button.remove();
+        item.remove();
+    })
 }
